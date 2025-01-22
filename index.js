b34d9b79b82efec4f0f4115cd1d273ae8e29d9e0
Kamila Nitta <kamila.nitta@gmail.com>
2025-01-22 10:36:05 +0100

    included JS

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const generatePoem = (event) => {
+  event.preventDefault()
 
+  new Typewriter("#poem", {
+    strings: "Hello Kamila",
+    delay: 1,
+    autoStart: true,
+    cursor: "",
+  })
+}
+
+let poemGenerator = document.querySelector("#generator-form")
+poemGenerator.addEventListener("submit", generatePoem)
