217325fb67aacb98166c8d454a5495bfbce3c38b
Kamila Nitta <kamila.nitta@gmail.com>
2025-01-22 11:53:16 +0100

    call api

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-const generatePoem = (event) => {
-  event.preventDefault()
-
+const displayPoem = (res) => {
   new Typewriter("#poem", {
-    strings: "Hello Kamila",
+    strings: res.data.answer,
     delay: 1,
     autoStart: true,
     cursor: "",
   })
+}
+
+const generatePoem = (event) => {
+  event.preventDefault()
+  let userInstructions = document.querySelector("#user-instructions")
+
+  let apiKey = "93b55a8f5451ff836432to73a1c94b02"
+  let context =
+    "Be sweet, short poems in around 4 lines, split poems lines with <br/> and be sweet"
+  let prompt = `Write beautiful poem with the word ${userInstructions.value}`
+  let url = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`
+
+  axios.get(url).then(displayPoem)
 }
 
 let poemGenerator = document.querySelector("#generator-form")
